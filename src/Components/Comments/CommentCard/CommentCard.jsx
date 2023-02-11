2e09633dc99c241abe26c1f5e634d35902d6d5ba
csturgeon <sturgeon.chris@gmail.com>
2023-02-11 08:36:15 +0000

    Add comment delete fetch and fade out animation

--- src/Components/Comments/CommentCard/CommentCard.jsx
+++ src/Components/Comments/CommentCard/CommentCard.jsx
@@ -3,7 +3,44 @@ import { useState } from 'react';
 import { format } from 'date-fns';
 import { sanitize } from 'dompurify';
 import { Link } from 'react-router-dom';
+import { motion, AnimatePresence } from 'framer-motion';
+
+// Component imports
 import LikeForm from '../../LikeForm/LikeForm';
+import CommentDeleteForm from '../CommentDeleteForm/CommentDeleteForm';
+
+const expandDown = {
+  initial: {
+    height: 0,
+    opacity: 0,
+  },
+  animate: {
+    height: 'auto',
+    opacity: 1,
+    transition: {
+      height: {
+        duration: 0.2,
+        delay: 1,
+      },
+      opacity: {
+        duration: 0.3,
+        delay: 1,
+      },
+    },
+  },
+  exit: {
+    height: 0,
+    opacity: 0,
+    transition: {
+      height: {
+        duration: 0.5,
+      },
+      opacity: {
+        duration: 0.2,
+      },
+    },
+  },
+};
 
 export default function CommentCard({ comment }) {
   const {
@@ -23,39 +60,51 @@ export default function CommentCard({ comment }) {
   const [liked, setLiked] = useState(hasLiked);
   const [totalLikes, setTotalLikes] = useState(likesCount);
   const imgURL = comment.userDetails[0].thumbURL;
+  const [commentIsVisible, setCommentIsVisible] = useState(true);
 
   return (
-    <div className="comment-card">
-      <div className="card-top">
-        <div className="comment-card-username">
-          <Link to={profileURL}>
-            <span>
-              {imgURL && <img src={imgURL} alt="profile"></img>}
-              <div>{comment.username}</div>
-            </span>
-          </Link>
-        </div>
-        <div className="comment-card-date">
-          {formattedDate} at {formattedTime}
-        </div>
-      </div>
-
-      <div className="card-bottom">
-        <div
-          className="comment-card-text"
-          dangerouslySetInnerHTML={{ __html: sanitize(text) }}
-        ></div>
-
-        <LikeForm
-          type={'comment'}
-          sessionID={sessionID}
-          commentID={commentID}
-          liked={liked}
-          setLiked={setLiked}
-          totalLikes={totalLikes}
-          setTotalLikes={setTotalLikes}
-        />
-      </div>
-    </div>
+    <AnimatePresence mode="wait" initial={false}>
+      {commentIsVisible && (
+        <motion.div
+          initial={expandDown.initial}
+          animate={expandDown.animate}
+          exit={expandDown.exit}
+          className="comment-card"
+        >
+          <div className="card-top">
+            <div className="comment-card-username">
+              <Link to={profileURL}>
+                <span>
+                  {imgURL && <img src={imgURL} alt="profile"></img>}
+                  <div>{comment.username}</div>
+                </span>
+              </Link>
+              <CommentDeleteForm
+                commentID={commentID}
+                setCommentIsVisible={setCommentIsVisible}
+              />
+            </div>
+            <div className="comment-card-date">
+              {formattedDate} at {formattedTime}
+            </div>
+          </div>
+          <div className="card-bottom">
+            <div
+              className="comment-card-text"
+              dangerouslySetInnerHTML={{ __html: sanitize(text) }}
+            ></div>
+            <LikeForm
+              type={'comment'}
+              sessionID={sessionID}
+              commentID={commentID}
+              liked={liked}
+              setLiked={setLiked}
+              totalLikes={totalLikes}
+              setTotalLikes={setTotalLikes}
+            />
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 }
---
+++ src/Components/Comments/CommentDeleteForm/CommentDeleteForm.jsx
@@ -0,0 +1,57 @@
+import './CommentDeleteForm.css';
+import { useState } from 'react';
+import { APIURL } from '../../../api';
+
+export default function CommentDeleteForm({ commentID, setCommentIsVisible }) {
+  const [formVisible, setFormVisible] = useState(false);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const url = `${APIURL}/comments/${commentID}`;
+
+    try {
+      const response = await fetch(url, {
+        method: 'DELETE',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `bearer ${window.localStorage.getItem('JWT')}`,
+        },
+      });
+
+      if (response.status === 200) {
+        setCommentIsVisible(false);
+      } else {
+        console.log('An error has occured');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  if (!formVisible) {
+    return (
+      <button
+        onClick={() => setFormVisible(true)}
+        className="comment-delete activate-btn"
+      >
+        <span>
+          <ion-icon name="trash-outline"></ion-icon>
+          <div>Remove</div>
+        </span>
+      </button>
+    );
+  }
+
+  return (
+    <div className="comment-delete confirm-cancel">
+      <form onSubmit={(event) => handleSubmit(event)}>
+        <button className="confirm-btn">Confirm</button>
+      </form>
+      <button onClick={() => setFormVisible(false)} className="cancel-btn">
+        Cancel
+      </button>
+    </div>
+  );
+}
